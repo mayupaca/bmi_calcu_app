ac3003eddfa3491b0e610bfc5cfeee618036f746
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-02-11 16:08:49 -0500

    define caluculator function

--- bmi_calcu_app/Bmi_calcu_app.py
+++ bmi_calcu_app/Bmi_calcu_app.py
@@ -4,9 +4,21 @@
 # You may choose to display it as a pie chart(percentage of people overweight/underweight/normal/obese)or bar chart.
 # BMI ＝ 体重kg ÷ (身長m)2
 
+import csv
 import tkinter
-from tkinter import ttk
 from tkinter import messagebox
+
+def calculator():
+    usr_height = int(height_txt.get())
+    usr_weight = int(weight_txt.get())
+    if usr_height == "" or usr_weight == "":
+        tkinter.messagebox.showwarning(title="Error", message="Height and weight cannot be blank.")
+    else:
+        with open("bmilist.csv", "a") as bmi_file:
+            bmi = usr_weight / (usr_height * usr_height)
+            bmi_file.write(str(bmi))
+        tkinter.messagebox.showinfo(title="Success", message="Saved")
+
 
 window = tkinter.Tk()
 window.title('BMI Calculator')
@@ -14,18 +26,20 @@ frame = tkinter.Frame(window)
 frame.pack()
 
 # ---------- User info ----------
-user_info = tkinter.LabelFrame(frame, text='User Information')
+user_info = tkinter.LabelFrame(frame, text='User Information', foreground="white", background="black")
 user_info.grid(row=0, column=0)
 
-height_label = tkinter.Label(user_info, text='Height: ')
+height_label = tkinter.Label(user_info, text='Height (m): ', foreground="white", background="black")
 height_label.grid(row=0, column=0, padx=10, pady=10)
 height_txt = tkinter.Entry(user_info)
 height_txt.grid(row=0, column=1, padx=10, pady=10)
 
-weight_label = tkinter.Label(user_info, text='weight: ')
+weight_label = tkinter.Label(user_info, text='weight (kg): ', foreground="white", background="black")
 weight_label.grid(row=1, column=0, padx=10, pady=10)
 weight_txt = tkinter.Entry(user_info)
 weight_txt.grid(row=1, column=1, padx=10, pady=10)
 
+calcu_button = tkinter.Button(frame, text="Calculate", command=calculator, foreground="white", background="black")
+calcu_button.grid(row=2,column=0)
 
 window.mainloop()
